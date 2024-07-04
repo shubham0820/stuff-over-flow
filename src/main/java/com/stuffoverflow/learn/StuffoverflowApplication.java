ae2ac439d7f2a32f22b90595f513ff33ecf6abe1
shubham <shubhammishra94@gmail.com>
2024-07-05 00:43:11 +0530

    [Implementation]: initializing app & configuring db connectivities

---
+++ src/main/java/com/stuffoverflow/learn/StuffoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.stuffoverflow.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuffoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuffoverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stuffoverflow/learn/StuffoverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuffoverflow.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuffoverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
